1e0e477f5dac4f1e054302ce51b9fdf3c28ae0c3
Gerardo Zayas <jerry@zayas.com>
2017-06-10 01:36:34 -0400

    0. Removed initial dictionary and changed first comment. 1. Added - Individual dictionaries collecting each player data. 2. Added - All player data collected in a single dictionary. 3. Added - Team variables to store players in separate teams.

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,22 +1,30 @@
-// Project 1 - Soccer Coordinator
+// Project 1 - Soccer League Coordinator
 
-// Dictionary [String: Any] containing all 18 players and their information
-let players = (["Name": "Joe Smith", "Height": 42, "Experience": true, "Guardian": "Jim and Jan Smith"],
-               ["Name": "Jill Tanner", "Height": 36, "Experience": true, "Guardian": "Clara Tanner"],
-               ["Name": "Bill Bon", "Height": 43, "Experience": true, "Guardian": "Sara and Jenny Bon"],
-               ["Name": "Eva Gordon", "Height": 45, "Experience": false, "Guardian": "Wendy and Mike Gordon"],
-               ["Name": "Matt Gill", "Height": 40, "Experience": false, "Guardian": "Charles and Sylvia Gill"],
-               ["Name": "Kimmy Stein", "Height": 41, "Experience": false, "Guardian": "Bill and Hillary Stein"],
-               ["Name": "Sammy Adams", "Height": 45, "Experience": false, "Guardian": "Jeff Adams"],
-               ["Name": "Karl Saygan", "Height": 42, "Experience": true, "Guardian": "Heather Bledsoe"],
-               ["Name": "Suzane Greenberg", "Height": 44, "Experience": true, "Guardian": "Henrietta Dumas"],
-               ["Name": "Sal Dali", "Height": 41, "Experience": false, "Guardian": "Gala Dali"],
-               ["Name": "Joe Kavalier", "Height": 39, "Experience": false, "Guardian": "Sam and Elaine Kavalier"],
-               ["Name": "Ben Finkelstein", "Height": 44, "Experience": false, "Guardian": "Aaron and Jill Finkelstein"],
-               ["Name": "Diego Soto", "Height": 41, "Experience": true, "Guardian": "Robin and Sarika Soto"],
-               ["Name": "Chloe Alaska", "Height": 47, "Experience": false, "Guardian": "David and Jamie Alaska"],
-               ["Name": "Arnold Willis", "Height": 43, "Experience": false, "Guardian": "Claire Willis"],
-               ["Name": "Phillip Helm", "Height": 44, "Experience": true, "Guardian": "Thomas Helm and Eva Jones"],
-               ["Name": "Les Clay", "Height": 42, "Experience": true, "Guardian": "Wynonna Brown"],
-               ["Name": "Herschel Krustofski", "Height": 45, "Experience": true, "Guardian": "Hyman and Rachel Krustofski"])
+// Individual dictionaries collecting each player data
+let player1: [String: Any]  = ["Name": "Joe Smith", "Height": 42, "Experience": true, "Guardian": "Jim and Jan Smith"]
+let player2: [String: Any]  = ["Name": "Jill Tanner", "Height": 36, "Experience": true, "Guardian": "Clara Tanner"]
+let player3: [String: Any]  = ["Name": "Bill Bon", "Height": 43, "Experience": true, "Guardian": "Sara and Jenny Bon"]
+let player4: [String: Any]  = ["Name": "Eva Gordon", "Height": 45, "Experience": false, "Guardian": "Wendy and Mike Gordon"]
+let player5: [String: Any]  = ["Name": "Matt Gill", "Height": 40, "Experience": false, "Guardian": "Charles and Sylvia Gill"]
+let player6: [String: Any]  = ["Name": "Kimmy Stein", "Height": 41, "Experience": false, "Guardian": "Bill and Hillary Stein"]
+let player7: [String: Any]  = ["Name": "Sammy Adams", "Height": 45, "Experience": false, "Guardian": "Jeff Adams"]
+let player8: [String: Any]  = ["Name": "Karl Saygan", "Height": 42, "Experience": true, "Guardian": "Heather Bledsoe"]
+let player9: [String: Any]  = ["Name": "Suzane Greenberg", "Height": 44, "Experience": true, "Guardian": "Henrietta Dumas"]
+let player10: [String: Any] = ["Name": "Sal Dali", "Height": 41, "Experience": false, "Guardian": "Gala Dali"]
+let player11: [String: Any] = ["Name": "Joe Kavalier", "Height": 39, "Experience": false, "Guardian": "Sam and Elaine Kavalier"]
+let player12: [String: Any] = ["Name": "Ben Finkelstein", "Height": 44, "Experience": false, "Guardian": "Aaron and Jill Finkelstein"]
+let player13: [String: Any] = ["Name": "Diego Soto", "Height": 41, "Experience": true, "Guardian": "Robin and Sarika Soto"]
+let player14: [String: Any] = ["Name": "Chloe Alaska", "Height": 47, "Experience": false, "Guardian": "David and Jamie Alaska"]
+let player15: [String: Any] = ["Name": "Arnold Willis", "Height": 43, "Experience": false, "Guardian": "Claire Willis"]
+let player16: [String: Any] = ["Name": "Phillip Helm", "Height": 44, "Experience": true, "Guardian": "Thomas Helm and Eva Jones"]
+let player17: [String: Any] = ["Name": "Les Clay", "Height": 42, "Experience": true, "Guardian": "Wynonna Brown"]
+let player18: [String: Any] = ["Name": "Herschel Krustofski", "Height": 45, "Experience": true, "Guardian": "Hyman and Rachel Krustofski"]
 
+// All player data collected in a single dictionary
+var players = [player1, player2, player3, player4, player5, player6, player7, player8, player9,
+               player10, player11, player12, player13, player14, player15, player16, player17, player18]
+
+// Team variables to store players in separate teams
+var teamSharks  = ""
+var teamDragons = ""
+var teamRaptors = ""
