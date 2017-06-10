f26a9af414d6761ff1b87d42612c97b6421e5eff
Gerardo Zayas <jerry@zayas.com>
2017-06-09 20:46:48 -0400

    Cleaned up dictionary by removing '[String: Any]' and added comment describing the dictionary.

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,22 +1,22 @@
 // Project 1 - Soccer Coordinator
 
-let players: ([String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any], [String: Any]) = (
-    ["Name": "Joe Smith", "Height": 42, "Experience": true, "Guardian": "Jim and Jan Smith"],
-    ["Name": "Jill Tanner", "Height": 36, "Experience": true, "Guardian": "Clara Tanner"],
-    ["Name": "Bill Bon", "Height": 43, "Experience": true, "Guardian": "Sara and Jenny Bon"],
-    ["Name": "Eva Gordon", "Height": 45, "Experience": false, "Guardian": "Wendy and Mike Gordon"],
-    ["Name": "Matt Gill", "Height": 40, "Experience": false, "Guardian": "Charles and Sylvia Gill"],
-    ["Name": "Kimmy Stein", "Height": 41, "Experience": false, "Guardian": "Bill and Hillary Stein"],
-    ["Name": "Sammy Adams", "Height": 45, "Experience": false, "Guardian": "Jeff Adams"],
-    ["Name": "Karl Saygan", "Height": 42, "Experience": true, "Guardian": "Heather Bledsoe"],
-    ["Name": "Suzane Greenberg", "Height": 44, "Experience": true, "Guardian": "Henrietta Dumas"],
-    ["Name": "Sal Dali", "Height": 41, "Experience": false, "Guardian": "Gala Dali"],
-    ["Name": "Joe Kavalier", "Height": 39, "Experience": false, "Guardian": "Sam and Elaine Kavalier"],
-    ["Name": "Ben Finkelstein", "Height": 44, "Experience": false, "Guardian": "Aaron and Jill Finkelstein"],
-    ["Name": "Diego Soto", "Height": 41, "Experience": true, "Guardian": "Robin and Sarika Soto"],
-    ["Name": "Chloe Alaska", "Height": 47, "Experience": false, "Guardian": "David and Jamie Alaska"],
-    ["Name": "Arnold Willis", "Height": 43, "Experience": false, "Guardian": "Claire Willis"],
-    ["Name": "Phillip Helm", "Height": 44, "Experience": true, "Guardian": "Thomas Helm and Eva Jones"],
-    ["Name": "Les Clay", "Height": 42, "Experience": true, "Guardian": "Wynonna Brown"],
-    ["Name": "Herschel Krustofski", "Height": 45, "Experience": true, "Guardian": "Hyman and Rachel Krustofski"])
+// Dictionary [String: Any] containing all 18 players and their information
+let players = (["Name": "Joe Smith", "Height": 42, "Experience": true, "Guardian": "Jim and Jan Smith"],
+               ["Name": "Jill Tanner", "Height": 36, "Experience": true, "Guardian": "Clara Tanner"],
+               ["Name": "Bill Bon", "Height": 43, "Experience": true, "Guardian": "Sara and Jenny Bon"],
+               ["Name": "Eva Gordon", "Height": 45, "Experience": false, "Guardian": "Wendy and Mike Gordon"],
+               ["Name": "Matt Gill", "Height": 40, "Experience": false, "Guardian": "Charles and Sylvia Gill"],
+               ["Name": "Kimmy Stein", "Height": 41, "Experience": false, "Guardian": "Bill and Hillary Stein"],
+               ["Name": "Sammy Adams", "Height": 45, "Experience": false, "Guardian": "Jeff Adams"],
+               ["Name": "Karl Saygan", "Height": 42, "Experience": true, "Guardian": "Heather Bledsoe"],
+               ["Name": "Suzane Greenberg", "Height": 44, "Experience": true, "Guardian": "Henrietta Dumas"],
+               ["Name": "Sal Dali", "Height": 41, "Experience": false, "Guardian": "Gala Dali"],
+               ["Name": "Joe Kavalier", "Height": 39, "Experience": false, "Guardian": "Sam and Elaine Kavalier"],
+               ["Name": "Ben Finkelstein", "Height": 44, "Experience": false, "Guardian": "Aaron and Jill Finkelstein"],
+               ["Name": "Diego Soto", "Height": 41, "Experience": true, "Guardian": "Robin and Sarika Soto"],
+               ["Name": "Chloe Alaska", "Height": 47, "Experience": false, "Guardian": "David and Jamie Alaska"],
+               ["Name": "Arnold Willis", "Height": 43, "Experience": false, "Guardian": "Claire Willis"],
+               ["Name": "Phillip Helm", "Height": 44, "Experience": true, "Guardian": "Thomas Helm and Eva Jones"],
+               ["Name": "Les Clay", "Height": 42, "Experience": true, "Guardian": "Wynonna Brown"],
+               ["Name": "Herschel Krustofski", "Height": 45, "Experience": true, "Guardian": "Hyman and Rachel Krustofski"])
 
